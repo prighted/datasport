8d2f40eb9b89a8d34c36b8ba0fc589772d1ca230
reinaldopabonfuentes <reinaldopabonfuentes@outlook.com>
2015-09-02 10:32:08 -0500

    prueba

--- DataSport/src/datasport/Conexion.java
+++ DataSport/src/datasport/Conexion.java
@@ -9,13 +9,6 @@ package datasport;
  *
  * @author Reinaldo Pabon
  */
-public class DataSport {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
+public class Conexion {
     
 }
