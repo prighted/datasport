a5c19117f7aa9edfe176f1cf8b3868fa8d0eaf9a
reinaldopabonfuentes <reinaldopabonfuentes@outlook.com>
2015-09-02 15:52:05 -0500

    Definición parámetros y métodos vacíos de ProgramaEjercicio

--- DataSport/src/datasport/ProgramaEjercicio.java
+++ DataSport/src/datasport/ProgramaEjercicio.java
@@ -11,4 +11,55 @@ package datasport;
  */
 public class ProgramaEjercicio {
     
+   private float vel;
+   private float inc;
+   private int vuelta;
+   private float calorias; /*No se si sea float 
+                            o sea int, por favor revisar
+                            */
+
+    public float getVel() {
+        return vel;
+    }
+
+    public float getInc() {
+        return inc;
+    }
+
+    public int getVuelta() {
+        return vuelta;
+    }
+
+    public float getCalorias() {
+        return calorias;
+    }
+   
+    public float calcularKm(float vel, time tiempo ) //Falta colocar el tiempo
+    {
+      return 1;                                      /* Se coloca 1 para evitar 
+                                                        obtener errores
+                                                      */
+    }
+    
+    public int calcularVueltas(float vel, time tiempo)
+    {
+      return 1;
+              
+    }
+    
+    public int calculaTimeLap()
+    {
+          return 1;
+    }
+    
+    public float calAcum(float cal)
+    {
+        return 1;
+    }
+    
+    public float calcularCal(float vel, float inc)
+    {
+        return 1;
+    }
+    
 }
