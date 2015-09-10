154f5fcaff32d93ff44db44ba3e573b4c32112d5
reinaldopabonfuentes <reinaldopabonfuentes@outlook.com>
2015-09-10 13:18:47 -0500

    Se añade el Reloj de la buena forma.

--- DataSport/src/datasport/ActualizarReloj.java
+++ DataSport/src/datasport/ActualizarReloj.java
@@ -6,33 +6,35 @@
 package datasport;
 
 import javax.swing.JLabel;
+
 /**
  *
  * @author Reinaldo Pabon
  */
-public class ActualizarReloj implements Runnable{
+public class ActualizarReloj implements Runnable {
     //Codigo que se ejecutara durante el thread
-  
-   private JLabel etiqueta;
-   Relojrun running = new Relojrun();
-   public ActualizarReloj(JLabel label)
-   {
-   etiqueta=label;
-   }
-   @Override
-    public void run()
-   {
-       try {
-           
-          
-               String hora = running.getHoraString();
-               etiqueta.setText(hora);
-               Thread.sleep(1000);
-           
-          
-           
-       } catch (InterruptedException ex){};
-           
-       
-   }
+
+    private JLabel etiqueta;
+    Relojrun running = new Relojrun();
+    public boolean vivo;
+
+    public ActualizarReloj(JLabel label) {
+        etiqueta = label;
+        vivo = true;
+    }
+
+    @Override
+    public void run() {
+        while (vivo) {
+            try {
+                  
+                String hora = running.getHoraString();
+                etiqueta.setText(hora);
+                Thread.sleep(1000);
+
+            } catch (InterruptedException ex) {
+            };
+        }
+
+    }
 }
--- DataSport/src/datasport/Relojrun.java
+++ DataSport/src/datasport/Relojrun.java
@@ -4,28 +4,34 @@
  * and open the template in the editor.
  */
 package datasport;
+
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 
 /**
- * Esta clase solo es empleada para obtener informacion de la hora (ACA NO EXISTE EL TIEMPO)
+ * Esta clase solo es empleada para obtener informacion de la hora (ACA NO
+ * EXISTE EL TIEMPO)
+ *
  * @author Reinaldo Pabon
  */
 public class Relojrun {
     /*
-    Variables para obtener la hora.
-    */
-       Calendar cal=Calendar.getInstance();
-       int segundos = cal.get(cal.SECOND);
-       int minutos = cal.get(cal.MINUTE);
-       int horas = cal.get(cal.HOUR_OF_DAY);
-    public String getHoraString ()
-    {
-     String hora=horas+":"+minutos+":"+segundos;
-      return hora;
-    }
+     Variables para obtener la hora.
+     */
 
+
+    public String getHoraString() {
+
+        Calendar cal = Calendar.getInstance();
+        int segundos = cal.get(cal.SECOND);
+        int minutos = cal.get(cal.MINUTE);
+        int horas = cal.get(cal.HOUR_OF_DAY);
+        String hora = horas + ":" + minutos + ":" + segundos;
+        return hora;
+    }
+/**
+  
     public int getSegundos() {
         return segundos;
     }
@@ -37,6 +43,5 @@ public class Relojrun {
     public int getHoras() {
         return horas;
     }
-    
-    
+*/
 }
