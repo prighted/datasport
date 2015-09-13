5eab4d40c1c04649595fd0cb898c7969c80276b5
rafael <rafael@rafael-pc>
2015-09-13 12:34:33 -0500

    conexion hecha con la base de datos (prueba) falta hacerlo con datasport, métodos y demás

--- DataSport/src/datasport/Conexion.java
+++ DataSport/src/datasport/Conexion.java
@@ -13,27 +13,35 @@ import java.sql.*;
  */
 public class Conexion {
     
-    public void conectaraBD(){
-      try{
-          
-          String controlador = "org.sqlite.JDBC";
-          String cadenaconexion = "jdbc:sqlite:BDdatasport1";
-          
-          Class.forName(controlador);
-          Connection cn;
-          cn = DriverManager.getConnection(cadenaconexion);
-                    
-      }catch(ClassNotFoundException ex){
-      System.out.println("Controlador no válido");
-      }catch (SQLException ex){
-      ex.printStackTrace();
-      }
+    public static void main(String[] args) {
+        try {
+ 
+            Class.forName("org.sqlite.JDBC");
+            Connection cn;
+            // "jdbc:sqlite:*** remplazar los asteríscos por la ruta de la base de datos en su pc.
+            //Ejemplo: jdbc:sqlite:c:/usuarios/rafael/netbeansproyects/datasport/src/datasport/bdatasport.db"
+            cn = DriverManager.getConnection("jdbc:sqlite:/home/rafael/NetBeansProjects/datasport/DataSport/src/datasport/BDdatasport1");
+            Statement st =cn.createStatement();
+            ResultSet rs= st.executeQuery("select * from ejercicio");
+ 
+            while (rs.next()){
+                System.out.print("Usuario: "+rs.getString("usuario"));
+                System.out.println(" Calorias: "+rs.getString("calorias"));
+                System.out.println(" Calorias Acumuladas: "+rs.getString("calAcumuladas"));
+                System.out.println(" CI: "+rs.getString("CI"));
+                System.out.println(" CV: "+rs.getString("CV"));
+                System.out.println(" Inclinacion: "+rs.getString("inc"));
+                System.out.println(" Tiempo: "+rs.getString("tiempo"));
+                System.out.println(" Velocidad: "+rs.getString("vel"));
+                System.out.println(" Vuelta: "+rs.getString("vuelta"));
+                
+            }
+ 
+        } catch (ClassNotFoundException ex) {
+            System.out.println("Controlador no válido");
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+ 
     }
-    
-    public void guardarenBD(){
-    
-        
-        
-    }
-    
 }
