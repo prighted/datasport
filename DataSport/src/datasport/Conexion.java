d68bfaf005e8f99eb21aa4acfac854bf7338404d
rafael <rafael@rafael-pc>
2015-09-13 01:17:02 -0500

    preparando la clase de conexion a la bd

--- DataSport/src/datasport/Conexion.java
+++ DataSport/src/datasport/Conexion.java
@@ -5,10 +5,35 @@
  */
 package datasport;
 
+import java.sql.*;
+
 /**
  *
  * @author Reinaldo Pabon
  */
 public class Conexion {
+    
+    public void conectaraBD(){
+      try{
+          
+          String controlador = "org.sqlite.JDBC";
+          String cadenaconexion = "jdbc:sqlite:BDdatasport1";
+          
+          Class.forName(controlador);
+          Connection cn;
+          cn = DriverManager.getConnection(cadenaconexion);
+                    
+      }catch(ClassNotFoundException ex){
+      System.out.println("Controlador no válido");
+      }catch (SQLException ex){
+      ex.printStackTrace();
+      }
+    }
+    
+    public void guardarenBD(){
+    
+        
+        
+    }
     
 }
